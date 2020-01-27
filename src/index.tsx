d753a414ab29680ea39e16b1c5b957bb991d04a0
Kirk Chu <kkchu791@gmail.com>
2020-01-26 20:39:29 -0800

    Make font Brandon Grotesque

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-//import './index.css';
+import './index.css';
 //import './fonts/BrandonGrotesque-Regular.ttf'
 
 import { App } from "./App";
