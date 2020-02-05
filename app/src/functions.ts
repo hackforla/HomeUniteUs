1f9fc812a16ef2fd651aa10d9d5688f9393aac93
Tim Malstead <timmmalstead@gmail.com>
2020-02-04 22:52:43 -0800

    start data functions object

--- app/src/functions.ts
+++ app/src/functions.ts
@@ -1,13 +1,30 @@
 const dataFunc = {
-    async get(params: string) {
+    async getGuests() {
         try {
-            const getUsers = await fetch(`http://0.0.0.0:8765/users/${params}`)
+            const getUsers = await fetch(`http://0.0.0.0:8765/guests/`,
+                {
+                    method: "GET",
+                    headers: { 'Content-Type': 'application/json' }
+                })
             return await getUsers.json()
         }
         catch (err) {
-            console.log(err)
+            throw err
         }
-    }
+    },
+    async getGuest(params: string) {
+        try {
+            const getUser = await fetch(`http://0.0.0.0:8765/guests/${params}`,
+                {
+                    method: "GET",
+                    headers: { 'Content-Type': 'application/json' }
+                })
+            return await getUser.json()
+        }
+        catch (err) {
+            throw err
+        }
+    },
 }
 
 export default dataFunc
