e106a335922ecc46bf3b675f1e6935c7868dc840
Tim Malstead <timmmalstead@gmail.com>
2020-02-18 18:48:42 -0800

    starts host page

--- app/src/components/HostProfile/ButtonBar.tsx
+++ app/src/components/HostProfile/ButtonBar.tsx
@@ -1,91 +1,88 @@
-import * as React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import { useParams, useHistory } from 'react-router';
-import { useHostHomeData } from '../../data/data-context';
-import { ProgressPlugin } from 'webpack';
-import { Host, MatchResult } from '../../models';
+import * as React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Button from "@material-ui/core/Button"
+import { useParams, useHistory } from "react-router"
+import { useHostHomeData } from "../../data/data-context"
+import { ProgressPlugin } from "webpack"
+import { Host, MatchResult } from "../../models"
 
 const useStyles = makeStyles(theme => ({
   appBar: {
-    top: 'auto',
+    top: "auto",
     bottom: 0,
-    height: '50px',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    flexDirection: 'row',
-    alignItems: 'center',
+    height: "100px",
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
+    alignItems: "flex-start"
   },
   buttons: {
-    marginRight: "100px",
+    margin: "0 100px 0 35px"
   },
   yesButton: {
     marginLeft: "20px",
-    backgroundColor: "#00AAEF"
+    backgroundColor: "#00AAEF",
+    margin: "0 25px 0 0"
   },
   extraSpace: {
-    height: '50px',
+    height: "50px"
   }
-}));
+}))
 
 export const ButtonBar = () => {
+  const classes = useStyles()
+  const history = useHistory()
+  let { guestId, hostId } = useParams()
 
-  const classes = useStyles();
-  const history = useHistory();
-  let {guestId, hostId} = useParams();
+  const gid = Number.parseInt(guestId || "")
+  const hid = Number.parseInt(hostId || "")
 
-
-  const gid = Number.parseInt(guestId || '');
-  const hid = Number.parseInt(hostId || '');
-  
-  const {data, markAsInterested, markAsNotInterested} = useHostHomeData();
-
-  
+  const { data, markAsInterested, markAsNotInterested } = useHostHomeData()
 
   return (
     <React.Fragment>
       <AppBar position="fixed" color="inherit" className={classes.appBar}>
-
+        <div style={{ margin: "0 0 0 50px", fontWeight: "700" }}>
+          Would you like to express interest in this host's home?
+        </div>
         <div className={classes.buttons}>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => {
-
-              markAsNotInterested({
-                guestId: gid,
-                hostId: hid
-              });
-
-              history.push(`/hosthome/admin/guest/${guestId}`);
-
-            }}
-          >
-            I'm not interested
-          </Button>
           <Button
             variant="contained"
             color="primary"
             className={classes.yesButton}
-            onClick={() =>{
+            onClick={() => {
               markAsInterested({
                 guestId: gid,
                 hostId: hid
-              });
+              })
 
-              history.push(`/hosthome/admin/guest/${guestId}`);
-
+              history.push(`/hosthome/admin/guest/${guestId}`)
             }}
           >
             I'm interested
           </Button>
+          <Button
+            variant="contained"
+            // color="secondary"
+            onClick={() => {
+              markAsNotInterested({
+                guestId: gid,
+                hostId: hid
+              })
+
+              history.push(`/hosthome/admin/guest/${guestId}`)
+            }}
+            style={{ backgroundColor: "white" }}
+          >
+            I'm not interested
+          </Button>
         </div>
       </AppBar>
-      
+
       <div className={classes.extraSpace}></div>
     </React.Fragment>
-  );
+  )
 }
 
-export default ButtonBar;
+export default ButtonBar
--- app/src/components/HostProfile/HostAdditionalInfo.tsx
+++ app/src/components/HostProfile/HostAdditionalInfo.tsx
@@ -1,42 +1,38 @@
-import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Host } from '../../models/Host';
-import { Element } from 'react-scroll'
+import * as React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Host } from "../../models/Host"
+import { Element } from "react-scroll"
 
 const useStyles = makeStyles(() => ({
   additionalInfoContainer: {
-    padding: '10px 20px',
-    border: '1px hidden blue',
-    marginTop: '50px',
+    padding: "10px 20px",
+    border: "1px hidden blue",
+    marginTop: "50px"
   },
   additionalInfoTitle: {
-    fontSize: '24px',
-    border: '1px hidden blue',
+    fontSize: "24px",
+    border: "1px hidden blue"
   },
   additionalInfos: {
-    width: '70%',
-    border: '1px hidden orange',
+    width: "70%",
+    border: "1px hidden orange"
   },
   additionalInfo: {
-    border: '1px hidden orange',
-    margin: '20px 0',
+    border: "1px hidden orange",
+    margin: "20px 0"
   },
   additionalInfoLabel: {
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
-  additionalInfoContent: {
-
-  },
-}));
+  additionalInfoContent: {}
+}))
 
 interface IAdditionalInfoProps {
-  host: Host;
+  host: Host
 }
 
-export const HostAdditionalInfo = ({
-  host
-}: IAdditionalInfoProps) => {
-  const classes = useStyles();
+export const HostAdditionalInfo = ({ host }: IAdditionalInfoProps) => {
+  const classes = useStyles()
 
   const additionalInfos = [
     {
@@ -44,57 +40,57 @@ export const HostAdditionalInfo = ({
       content: `I'm currently employed at ${host.employmentCompany} as ${host.employmentPosition}`
     },
     {
-      label: "My passions are:",
-      content: host.interests.join(", ")
-    },
-    {
-      label: "Language(s) spoken:",
+      label: "Languages Spoken:",
       content: host.languages.join(", ")
     },
     {
-      label: "Additional preferences/desired characteristics:",
+      label: "My Passions Are:",
+      content: host.interests.join(", ")
+    },
+    {
+      label: "Desired Characteristics In A Tenant:",
       content: host.preferredCharacteristics
     },
     {
-      label: "I would like to be a host home because:",
+      label: "I Would Like To Be A Host Home Because:",
       content: host.hostingInterest
     },
     {
-      label: "I think I would be a good host because:",
+      label: "I Think I Would Be A Good Host Because:",
       content: host.hostingStrengths
     },
     {
-      label: "These are challenges I might face in being a good host:",
+      label: "These Are Challenges I May Face As A Host:",
       content: host.hostingChallenges
     },
     {
-      label: "More about me:",
+      label: "More About Me:",
       content: host.hostIntro
     }
   ]
 
   return (
-  
     <Element name="additionalInfo">
       <div className={classes.additionalInfoContainer}>
-        <div className={classes.additionalInfoTitle}>
-          Additional Info
-        </div>
+        <div className={classes.additionalInfoTitle}>About the Host</div>
 
         <div className={classes.additionalInfos}>
           {additionalInfos.map(additionalInfo => {
-
             return (
               <div className={classes.additionalInfo}>
-                <div className={classes.additionalInfoLabel}>{additionalInfo.label}</div>
-                <div className={classes.additionalInfoContent}>{additionalInfo.content}</div>
+                <div className={classes.additionalInfoLabel}>
+                  {additionalInfo.label}
+                </div>
+                <div className={classes.additionalInfoContent}>
+                  {additionalInfo.content}
+                </div>
               </div>
             )
           })}
         </div>
       </div>
     </Element>
-  );
+  )
 }
 
-export default HostAdditionalInfo;
+export default HostAdditionalInfo
