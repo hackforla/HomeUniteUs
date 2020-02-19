0806b54ba04e36b2e7edc967c0f80dd001b0ea48
Tim Malstead <timmmalstead@gmail.com>
2020-02-18 19:05:39 -0800

    starts host profile

--- app/src/components/HostProfile/HousePolicies.tsx
+++ app/src/components/HostProfile/HousePolicies.tsx
@@ -1,61 +1,60 @@
-import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaw } from "@fortawesome/free-solid-svg-icons";
-import { faSmokingBan } from "@fortawesome/free-solid-svg-icons";
-import { faWineBottle } from "@fortawesome/free-solid-svg-icons";
-import { faPrescriptionBottleAlt } from "@fortawesome/free-solid-svg-icons";
-import { faBaby } from "@fortawesome/free-solid-svg-icons";
-import { faUserFriends } from "@fortawesome/free-solid-svg-icons";
-import { Host } from '../../models/Host';
+import * as React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPaw } from "@fortawesome/free-solid-svg-icons"
+import { faSmokingBan } from "@fortawesome/free-solid-svg-icons"
+import { faWineBottle } from "@fortawesome/free-solid-svg-icons"
+import { faPrescriptionBottleAlt } from "@fortawesome/free-solid-svg-icons"
+import { faBaby } from "@fortawesome/free-solid-svg-icons"
+import { faUserFriends } from "@fortawesome/free-solid-svg-icons"
+import { Host } from "../../models/Host"
 
 const useStyles = makeStyles(() => ({
   housePolicyContainer: {
-    padding: '10px 20px',
-    border: '1px hidden red',
-    marginTop: '50px',
+    padding: "10px 20px",
+    border: "1px hidden red",
+    marginTop: "50px"
   },
   housePolicyTitle: {
-    fontSize: '24px',
-    border: '1px hidden green',
+    fontSize: "24px",
+    border: "1px hidden green"
   },
   housePolicies: {
-    border: '1px hidden black',
-    width: '70%',
-    padding: '20px 0',
-    display: 'flex',
-    flexWrap: 'wrap',
+    border: "1px hidden black",
+    width: "90%",
+    padding: "20px 0",
+    display: "flex",
+    flexWrap: "wrap"
   },
   policy: {
-    border: '1px hidden blue',
-    width: '49%',
-    margin: '20px 0',
+    border: "1px hidden blue",
+    width: "30%",
+    margin: "20px 0",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
-  icon: {
-
-  },
+  icon: {},
   content: {
-    paddingLeft: '10px',
+    paddingLeft: "10px"
   }
-}));
+}))
 
 interface IHousePoliciesProps {
-  host: Host;
+  host: Host
 }
 
-export const HousePolicies = ({
-  host,
-}: IHousePoliciesProps) => {
-  const classes = useStyles();
+export const HousePolicies = ({ host }: IHousePoliciesProps) => {
+  const classes = useStyles()
 
   let policies = [
     {
       icon: faPaw,
-      content: host.petsText,
+      content: host.petsText
     },
     {
       icon: faSmokingBan,
-      content: host.smokingText,
+      content: host.smokingText
     },
     {
       icon: faWineBottle,
@@ -63,34 +62,27 @@ export const HousePolicies = ({
     },
     {
       icon: faPrescriptionBottleAlt,
-      content: host.substancesText,
+      content: host.substancesText
     }
   ]
 
   if (host.youthParenting) {
-    policies = policies.concat(
-      {
-        icon: faBaby,
-        content: "We welcome parents."
-      }
-    )
+    policies = policies.concat({
+      icon: faBaby,
+      content: "We welcome parents."
+    })
   }
 
   if (host.youthRelationship) {
-    policies = policies.concat(
-      {
-        icon: faUserFriends,
-        content: "We welcome youths in relationships."
-      }
-    )
+    policies = policies.concat({
+      icon: faUserFriends,
+      content: "We welcome youths in relationships."
+    })
   }
 
   return (
     <div className={classes.housePolicyContainer}>
-
-      <div className={classes.housePolicyTitle}>
-        House Policy
-      </div>
+      <div className={classes.housePolicyTitle}>House Policy</div>
 
       <div className={classes.housePolicies}>
         {policies.map(policy => {
@@ -100,16 +92,16 @@ export const HousePolicies = ({
                 <FontAwesomeIcon
                   icon={policy.icon}
                   aria-hidden="true"
-                  size="sm"
+                  size="7x"
                 />
               </span>
-              <span className={classes.content}>{policy.content}</span>
+              <p className={classes.content}>{policy.content}</p>
             </div>
           )
         })}
       </div>
     </div>
-  );
+  )
 }
 
-export default HousePolicies;
+export default HousePolicies
