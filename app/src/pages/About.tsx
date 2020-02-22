6349a007353b8c5d5d55324aebe163c7dc70fa2e
Tim Malstead <timmmalstead@gmail.com>
2020-02-22 12:40:35 -0800

    preps for pull

--- app/src/pages/About.tsx
+++ app/src/pages/About.tsx
@@ -1,27 +1,31 @@
-import * as React from 'react';
-import { Container, Paper, Typography, makeStyles } from '@material-ui/core';
+import * as React from "react"
+import { Container, Paper, Typography, makeStyles } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   sidebarAboutBox: {
     padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[200],
+    backgroundColor: theme.palette.grey[200]
   },
   sidebarSection: {
-    marginTop: theme.spacing(3),
-  },
-}));
+    marginTop: theme.spacing(3)
+  }
+}))
 
-
-const firstParagraph = "Welcome to Hack for LA's host homes project! Hack for LA is a brigade of Code for America, and we're working with our partners at Safe Place for Youth (SPY) to enhance their host homes program. Together, we're developing a workflow management tool, digitizing key moments within SPY’s host homes initiative to make the process scaleable, reduce institutional bias, and effectively capture data.";
-const secondParagraph = "SPY’s host homes program is centered around housing young people, 18 - 25 years old. Their approach focuses on low-cost, community-driven intervention by matching a willing host with a guest or group of guests, providing a stable housing environment for youths who are experiencing homelessness and seeking stable housing.";
-const thirdParagraph = "We're currently seeking additional stakeholders who run similar host home/empty bedroom programs, to provide input and feedback on how this tool can and could be used. We're also seeking organizations and individuals interested in supporting the project financially."
+const firstParagraph =
+  "Welcome to Hack for LA's host homes project! Hack for LA is a brigade of Code for America, and we're working with our partners at Safe Place for Youth (SPY) to enhance their host homes program. Together, we're developing a workflow management tool, digitizing key moments within SPY’s host homes initiative to make the process scaleable, reduce institutional bias, and effectively capture data."
+const secondParagraph =
+  "SPY’s host homes program is centered around housing young people, 18 - 25 years old. Their approach focuses on low-cost, community-driven intervention by matching a willing host with a guest or group of guests, providing a stable housing environment for youths who are experiencing homelessness and seeking stable housing."
+const thirdParagraph =
+  "We're currently seeking additional stakeholders who run similar host home/empty bedroom programs, to provide input and feedback on how this tool can and could be used. We're also seeking organizations and individuals interested in supporting the project financially."
 
 export const AboutPage = () => {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <Container>
       <Paper elevation={0} className={classes.sidebarAboutBox}>
-        <Typography variant="h2" gutterBottom align='center'>About</Typography>
+        <Typography variant="h2" gutterBottom align="center">
+          About
+        </Typography>
         <Typography>
           <h4>Introduction</h4>
           <p>{firstParagraph}</p>
@@ -30,16 +34,18 @@ export const AboutPage = () => {
           <h4>Status</h4>
           <p>{thirdParagraph}</p>
           <p>
-            If either of these applies to you, please contact Bonnie Wolfe, Hack for LA's Executive Director
+            If either of these applies to you, please contact Bonnie Wolfe, Hack
+            for LA's Executive Director
             <a
               target="_blank"
               href="mailto:bonnie@hackforla.org?subject=Host Homes"
-              style={{ color: "black", margin: "0 0 0 .5em" }}>
+              style={{ color: "black", margin: "0 0 0 .5em" }}
+            >
               bonnie@hackforla.org
             </a>
           </p>
         </Typography>
       </Paper>
     </Container>
-  );
+  )
 }
