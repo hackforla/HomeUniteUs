3a201e30929f39bfae8b09b9ea3d7076015cb827
Tim Malstead <timmmalstead@gmail.com>
2020-02-26 20:30:34 -0800

    works further on guest profiles

--- app/src/components/GuestProfile/GuestCard.tsx
+++ app/src/components/GuestProfile/GuestCard.tsx
@@ -1,50 +1,56 @@
-import * as React from 'react';
-import { Guest } from '../../models/Guest'
+import * as React from "react"
+import { Guest } from "../../models/Guest"
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(() => ({
-    guestCardContainer: {
-        height: "400px",
-        padding: "10px 20px",
-        flexGrow: 1,
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "flex-start"
-
-    },
-    guestImageContainer: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    guestImage: {
-
-        boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)"
-    }
+  guestCardContainer: {
+    height: "400px",
+    padding: "10px 20px",
+    flexGrow: 1,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  guestImageContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  guestImage: {
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)"
+  }
 }))
 
 interface Props {
-    guest: Guest
+  guest: Guest
 }
 
 const GuestCard = ({ guest }: Props) => {
+  const randomProfile = Math.floor(Math.random() * 4)
 
-    const style = useStyles()
+  const style = useStyles()
 
-    return (
-        <div className={style.guestCardContainer}>
-            <div className={style.guestImageContainer}>
-                <img
-                    className={style.guestImage}
-                    src={guest.id === 999 ? "/hosthome/img/kirk.png" : guest.id === 998 ? "/hosthome/img/megan.png" : "/hosthome/img/profile2.png"}
-                    alt={"Guest Image"}
-                    height="400px"
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.guestCardContainer}>
+      <div className={style.guestImageContainer}>
+        <img
+          className={style.guestImage}
+          src={
+            guest.id === 999
+              ? "/hosthome/img/kirk.png"
+              : guest.id === 998
+              ? "/hosthome/img/megan.png"
+              : `/hosthome/img/profile${randomProfile}.png`
+          }
+          alt={"Guest Image"}
+          height="400px"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default GuestCard
