f1442c42fb4bb8b7602db33b92998b8183768f49
arshiamasih <arshia.masih@gmail.com>
2020-03-24 16:38:42 -0700

    adds webpack dev server configuration to resolve react router routing and HMR

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '../build/dist'),
         filename: '[name].[hash:8].js',
+        publicPath: '/',
         // TODO: this line may need to be uncommented and changed for different webserver environments
         //     ...this prefix determines what is prepended to resource requests from "index.html"
         //     ...e.g. the value 'dist' in the injected tag <script src="/dist/react-dom.js"></script>
@@ -115,5 +116,9 @@ module.exports = {
             { from: './src/img/*.svg', to: 'hosthome/img', flatten: true },
 
         ]),
-    ]
+    ],
+    devServer: {
+        historyApiFallback: true,
+        publicPath: '/',
+    },
 };
