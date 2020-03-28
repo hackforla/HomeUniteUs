4b18ce239f419a5933663ede02ff873b8bc276db
tylerthome <tyler_thome@yahoo.com>
2020-03-28 12:40:11 -0700

    scaffold

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,63 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Auth0Provider } from './react-auth0-spa';
+import history from './utils/history';
+
+  
+const AUTH0_DOMAIN='dev-w3xfkh4k.auth0.com'
+const AUTH0_CLIENT_ID='Pe9Cr1hBBRjVH2gBzR9KWpF8xPGrDoyN'
+const AUTH0_AUDIENCE='dev-startup-boilerplate-api'
+
 import "./index.css"
 
 import { App } from "./App"
 
 window.addEventListener("load", function() {
   console.log(`window:load: about to render app...`)
-  ReactDOM.render(<App />, document.getElementById("app-root"))
-  console.log(`window:load: app rendered`)
+  
+  const auth0Domain = AUTH0_DOMAIN;
+  const auth0ClientId = AUTH0_CLIENT_ID;
+  const auth0RedirectUri = window.location.origin;
+  
+  // if (
+  //   auth0Domain === undefined
+  //   || auth0ClientId === undefined
+  //   || auth0Audience === undefined
+  // ) {
+  //   throw new Error('missing env vars');
+  // }
+  
+  const onAuthRedirectCallback = (redirectResult?: RedirectLoginResult) => {
+    console.log(
+      'auth0 onRedirectCallback called with redirectState %o',
+      redirectResult
+    );
+  
+    // Clears auth0 query string parameters from url
+    const targetUrl = redirectResult
+      && redirectResult.appState
+      && redirectResult.appState.targetUrl
+        ? redirectResult.appState.targetUrl
+        : window.location.pathname;
+        
+    history.push(targetUrl);
+  };
+  
+  ReactDOM.render(
+    (
+      <Auth0Provider
+        domain={auth0Domain}
+        client_id={auth0ClientId}
+        redirect_uri={auth0RedirectUri}
+        // audience={auth0Audience}
+        onRedirectCallback={onAuthRedirectCallback}
+      >
+        <App />
+      </Auth0Provider>
+    ),
+    document.getElementById('app-root')
+  );
+
+
+  console.log(`window:load: app rendered`);
 })
