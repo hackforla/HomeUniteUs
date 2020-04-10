f4e6bb5d9333593e8a2a227edd26fdac9d465262
Tim Malstead <timmmalstead@gmail.com>
2020-04-10 15:27:39 -0700

    commits before docker call tongiht

--- scripts/live-update-container.sh
+++ scripts/live-update-container.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#in app folder run  
+
+    # npm run build:docker
+
+#and then run 
+
+    # zsh ./start-container.sh
+
+#from root directory
+
 host_home_containers=$(docker ps -a | tail -n+2 | grep "^.*host\-home.*$" | awk '{print $1}')
 echo "About to copy new webpack bundles to container(s):"
 echo $host_home_containers
