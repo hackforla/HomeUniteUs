371defb9b5e1f648a2520766ede98f03538aa401
HassenH1 <hasansaid51@gmail.com>
2020-05-15 19:52:14 -0700

    testing something out

--- app/src/pages/GuestQuestions/GuestQuestions.tsx
+++ app/src/pages/GuestQuestions/GuestQuestions.tsx
@@ -1,11 +1,63 @@
 import React from 'react'
-import { Body, Container } from './style'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Body, Container, Header, FormContainer, Error, Label, FormControl } from './style'
+import * as Yup from 'yup'
+
+interface MyFormValues {
+  firstName: string;
+  lastName: string;
+}
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+})
+
+const onSubmit = (values: MyFormValues) => {
+  console.log(values, "<----on submit")
+}
 
 function GuestQuestions() {
+  const initialValues: MyFormValues = { firstName: '', lastName: '' };
   return (
     <Body>
       <Container>
-        <p>Hello world</p>
+        <Header>
+          <h1>Contact Information.</h1>
+        </Header>
+        <FormContainer>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <FormControl>
+                <Label htmlFor='firstName'>First Name</Label>
+                <Field type='text' id='firstName' name='firstName' />
+                <ErrorMessage
+                  name="firstName">
+                  {(msg) => (
+                    <Error>{msg}</Error>
+                  )}
+                </ErrorMessage>
+              </FormControl>
+
+              <FormControl>
+                <Label htmlFor='lastName'>Last Name</Label>
+                <Field type='text' id='lastName' name='lastName' />
+                <ErrorMessage
+                  name="lastName">
+                  {(msg) => (
+                    <Error>{msg}</Error>
+                  )}
+                </ErrorMessage>
+              </FormControl>
+
+              <button type="submit">Submit</button>
+            </Form>
+          </Formik>
+        </FormContainer>
       </Container>
     </Body>
   )
--- app/src/pages/GuestQuestions/style.ts
+++ app/src/pages/GuestQuestions/style.ts
@@ -3,12 +3,38 @@ import styled from 'styled-components'
 export const Body = styled.div`
   min-height: 86.5vh;
   padding: 0;
+  margin: 0;
   background-color: #fff;
   display: flex;
   justify-content: center;
 ` 
 
 export const Container = styled.div`
-  width: 741.5px;
-  background-color: red;
+  width: 65.03125rem;
+`
+
+export const Header = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  height: 28px
+`
+
+export const FormContainer = styled.div`
+  width: 65.03125rem;
+  background-color: #fff;
+`
+
+export const FormControl = styled.div`
+  margin-bottom: 20px;
+`
+
+export const Label = styled.label`
+  display: flex;
+  margin-bottom: 5px;
+`
+
+export const Error = styled.div`
+  color: red;
 `
