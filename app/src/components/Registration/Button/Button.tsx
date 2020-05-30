203fc6081053edcae4bb29651f9bdbd1076daee3
arshiamasih <arshia.masih@gmail.com>
2020-05-29 18:58:14 -0700

    individual folders for each component for scoped style sheets

---
+++ app/src/components/Registration/Button/Button.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        margin: {
+            margin: theme.spacing(1),
+        },
+    })
+)
+
+interface Props {
+    id: string
+    name: string
+    value: string
+    onClick: (event: object) => void
+    label: string
+}
+
+export default function ButtonSizes(props: Props) {
+    const classes = useStyles()
+    const { label, name, value, id } = props
+
+    return (
+        <div>
+            <div>
+                <Button
+                    variant="contained"
+                    name={name}
+                    id={id}
+                    value={value}
+                    size="large"
+                    color="primary"
+                    className={classes.margin}
+                >
+                    {label}
+                </Button>
+            </div>
+        </div>
+    )
+}
