bdd4b825ad3dd972e0d84b1a0a09ed79bf53c9ff
HassenH1 <hasansaid51@gmail.com>
2020-06-12 19:54:15 -0700

    brb

--- app/src/components/MUIModal/ConfirmationModal/ConfirmationModal.tsx
+++ app/src/components/MUIModal/ConfirmationModal/ConfirmationModal.tsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import MUIModal from '../MUIModal'
-import { styles } from './styles' //taking this out for now
+import { styles } from './styles'
 import Button from '@material-ui/core/Button'
 import { withStyles, WithStyles } from '@material-ui/core/'
 
-// interface ConfirmationModalProps { //taking this out for now
 interface ConfirmationModalProps extends WithStyles<typeof styles> {
+    confirmationText: string
     handleClose: () => void
     modalOpen: boolean
+    onConfirm: () => void
+    onCancel: () => void
 }
 
 function ConfirmationModal(props: ConfirmationModalProps) {
-    const { modalOpen, handleClose, classes } = props
-
-    const onConfirm = () => {
-        console.log('confirm then close')
-        handleClose()
-    }
-
-    const onCancel = () => {
-        handleClose()
-    }
+    const {
+        modalOpen,
+        handleClose,
+        onConfirm,
+        onCancel,
+        confirmationText,
+        classes,
+    } = props
 
     return (
-        <MUIModal handleClose={handleClose} modalOpen={modalOpen}>
+        <MUIModal handleClose={onCancel} modalOpen={modalOpen}>
             <h2 id="transition-modal-title" style={{ color: 'red' }}>
                 Confirmation Modal
             </h2>
-            <p>Confirmation text goes here!</p>
-            <button onClick={onConfirm}>Yes</button>
+            <p>{confirmationText}</p>
+            <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={onConfirm}
+            >
+                Yes
+            </Button>
             <Button
                 variant="contained"
                 color="secondary"
                 className={classes.button}
                 onClick={onCancel}
             >
-                Delete
+                No
             </Button>
         </MUIModal>
     )
