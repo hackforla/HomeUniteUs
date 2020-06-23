6fb71c91fb27e7c8c2ec5369b5b19a0c4d9bf8aa
arshiamasih <arshia.masih@gmail.com>
2020-06-22 18:23:52 -0700

    creates reducer, provider, and custom hook

--- app/src/data/host-context.tsx
+++ app/src/data/host-context.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
-import { QuestionType as HostQuestion } from '../models/QuestionType'
-import { ResponseValue as HostResponseValue } from '../models/ResponseValue'
+import { QuestionType } from '../models/QuestionType'
+import { ResponseValue } from '../models/ResponseValue'
 
 const HostDashboardContext = React.createContext({})
 
-interface HostDashboardState {
-    hostQuestions: Array<HostQuestion>
-    hostResponses: Array<HostResponseValue>
+interface HostDashboardData {
+    hostQuestions: Array<QuestionType>
+    hostResponses: Array<ResponseValue>
     loaderState: {
         loading: boolean
-        text: string
+        message: string
     }
 }
 
@@ -24,14 +24,62 @@ enum HostDashboardActionType {
 
 interface HostDashboardAction {
     type: HostDashboardActionType
-    payload?: HostDashboardState | Array<HostQuestion> | boolean | string
+    payload?: HostDashboardData | Array<QuestionType> | boolean | string
 }
 
-const initialState: HostDashboardState = {
+const initialState: HostDashboardData = {
     hostQuestions: [],
     hostResponses: [],
     loaderState: {
         loading: false,
-        text: '',
+        message: '',
     },
+}
+
+function hostDashboardReducer(
+    state: HostDashboardData,
+    action: HostDashboardAction
+): HostDashboardData {
+    switch (action.type) {
+        case HostDashboardActionType.BeginFetchQuestions: {
+            return {
+                ...state,
+                loaderState: {
+                    loading: true,
+                    message: action.payload as string,
+                },
+            }
+        }
+        default:
+            throw new Error(`Unsupported action: ${JSON.stringify(action)}`)
+    }
+}
+
+export function HostDashboardDataProvider(
+    props: React.PropsWithChildren<{}>
+): JSX.Element {
+    const [state, dispatch] = React.useReducer(
+        hostDashboardReducer,
+        initialState
+    )
+    const value = React.useMemo(() => [state, dispatch], [state])
+    return <HostDashboardContext.Provider value={value} {...props} />
+}
+
+export function useHostDashboardData() {
+    const context = React.useContext(HostDashboardContext)
+    if (!context) {
+        throw new Error(
+            'useHostDashboardData must be used within a HostDashboardProvider'
+        )
+    }
+
+    const [data, dispatch] = context as [
+        HostDashboardData,
+        React.Dispatch<HostDashboardAction>
+    ]
+
+    return {
+        data,
+    }
 }
