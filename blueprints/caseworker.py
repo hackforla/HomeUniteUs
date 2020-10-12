159b3cdcc832feb9a52fade573e3a9d7a28d0289
HassenH1 <hasansaid51@gmail.com>
2020-10-11 19:21:28 -0700

    added the skeleton methods for GET/POST/PUT/DELETE in caseworker.py

--- blueprints/caseworker.py
+++ blueprints/caseworker.py
@@ -4,3 +4,46 @@ from jinja2 import TemplateNotFound
 import pymongo
 from bson import ObjectId
 from config.constants import DB_NAME
+
+caseworker_api = Blueprint('caseworker_api', __name__,
+                          template_folder='templates')
+
+@caseworker_api.route('/api/<orgname>/caseworkers', methods=['GET'])
+def get_all_caseworkers(org): 
+  try:
+
+  except Exception as e:
+    return jsonify(error=str(e)), 404
+  # return all caseworkers for organization
+
+@caseworker_api.route('/api/<orgname>/caseworkers/<caseworker_id>', methods=['GET'])
+def get_caseworkers(org): 
+  try:
+    
+  except Exception as e:
+    return jsonify(error=str(e)), 404
+  # return all caseworkers for organization
+
+@caseworker_api.route('/api/<orgname>/caseworkers', methods=['POST'])
+def add_caseworker(org, caseworker_id): 
+  try:
+    
+  except Exception as e:
+    return jsonify(error=str(e)), 404
+  # add a new caseworker to an organization
+
+@caseworker_api.route('/api/<orgname>/caseworkers/<caseworker_id>', methods=['PUT'])
+def update_caseworker(org, caseworker_id): 
+  try:
+    
+  except Exception as e:
+    return jsonify(error=str(e)), 404
+  # update a caseworker by id
+
+@caseworker_api.route('/api/<orgname>/caseworkers/<caseworker_id>', methods=['PUT'])
+def uodate_caseworker(org, caseworker_id): 
+  try:
+    
+  except Exception as e:
+    return jsonify(error=str(e)), 404
+  # delete a caseworker by id
