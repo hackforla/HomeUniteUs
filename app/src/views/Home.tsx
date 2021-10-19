071d3b559e732a30037bcb42261530bf66f28375
Erik <eguntner2291@gmail.com>
2021-10-18 17:24:04 -0700

    MCreate HomeLink component and add host, guest, and coordinatour images

--- app/src/views/Home.tsx
+++ app/src/views/Home.tsx
@@ -1,42 +1,55 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { NavLink } from "react-router-dom";
+import { Box, styled } from "@mui/system";
 
-function TempBtn(props: { name: string }) {
-  return (
-    <Box
-      sx={{
-        padding: "3rem 5rem",
-        bgcolor: "#6cccff",
-      }}
-    >
-      <Typography>{props.name}</Typography>
-    </Box>
-  );
-}
+import { HomeLink } from "../components/common/HomeLink";
 
 export function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        flex: 1,
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        backgroundColor: "#249BE5",
-      }}
-    >
-      <NavLink to="/home/guest">
-        <TempBtn name="Guest" />
-      </NavLink>
-      <NavLink to="/home/host">
-        <TempBtn name="Host" />
-      </NavLink>
-      <NavLink to="/home/coordinator">
-        <TempBtn name="Coordinator" />
-      </NavLink>
-    </div>
+    <HomeContainer>
+      <Header variant="h1">Welcome to a Safe Place for Youth</Header>
+      <SubHeader variant="h2">Select a profile</SubHeader>
+      <LinkRow>
+        <HomeLink to="/home/host" name="Host">
+          <img
+            src="../img/host-icon.svg"
+            alt="Family standing in front of home"
+          />
+        </HomeLink>
+        <HomeLink to="/home/coordinator" name="Admin">
+          <img
+            src="../img/admin-icon.svg"
+            alt="Administrator working at desk"
+          />
+        </HomeLink>
+        <HomeLink to="/home/guest" name="Guest">
+          <img src="../img/guest-icon.svg" alt="Guest carrying luggage" />
+        </HomeLink>
+      </LinkRow>
+    </HomeContainer>
   );
 }
+
+const HomeContainer = styled("section")({
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  gap: "32px",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const LinkRow = styled("div")({
+  display: "flex",
+  gap: "2rem",
+});
+
+const Header = styled(Typography)({
+  fontSize: "3rem",
+  fontWeight: 600,
+});
+
+const SubHeader = styled(Typography)({
+  fontSize: "2rem",
+  fontWeight: 600,
+});
