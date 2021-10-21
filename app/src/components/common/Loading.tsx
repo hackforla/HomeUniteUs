800a8b69499b97fc8ec85e8834cdb3c93a7a1825
Erik <eguntner2291@gmail.com>
2021-10-21 16:47:46 -0700

    Replace loading spinner and center on page

--- app/src/components/common/Loading.tsx
+++ app/src/components/common/Loading.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import { styled } from "@mui/system";
+
 const loadingImg =
   "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
 export const Loading = () => (
-  <div className="spinner">
-    <img src={loadingImg} alt="Loading..." />
-  </div>
+  <LoadingContainer>
+    <CircularProgress size={60} />
+  </LoadingContainer>
 );
+
+const LoadingContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  width: "100vw",
+});
