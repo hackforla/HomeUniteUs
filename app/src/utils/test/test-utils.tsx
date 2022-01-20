29360a5633a4189d7e28b2aefab5a676c8e2a039
Erik <eguntner2291@gmail.com>
2022-01-19 17:41:12 -0800

    Setup custom render

---
+++ app/src/utils/test/test-utils.tsx
@@ -0,0 +1,19 @@
+import {ReactElement, FC} from 'react';
+import {render, RenderOptions} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {ThemeProvider} from '@mui/material/styles';
+import {HomeUniteUsTheme} from '../../theme';
+
+const AllTheProviders: FC = ({children}) => {
+  return <ThemeProvider theme={HomeUniteUsTheme}>{children}</ThemeProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => {
+  return render(ui, {wrapper: AllTheProviders, ...options});
+};
+
+export * from '@testing-library/react';
+export {customRender as render, userEvent};
