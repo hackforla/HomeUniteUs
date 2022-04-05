86a71094eca195d52cfb11bbda9dcc1df323e4cc
Erik <eguntner2291@gmail.com>
2022-04-05 16:26:51 -0700

    Configure empty store

---
+++ app/src/redux/store.ts
@@ -0,0 +1,6 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+export const store = configureStore({reducer: {}});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
