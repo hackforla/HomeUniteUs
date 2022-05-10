aef7f00486f37da60e4c9858d7384e517733020c
Erik <eguntner2291@gmail.com>
2022-05-10 16:51:42 -0700

    Create sign in controller

--- api/openapi_server/controllers/auth_controller.py
+++ api/openapi_server/controllers/auth_controller.py
@@ -1,9 +1,11 @@
+from tkinter import E
 import connexion
 import boto3
 import hmac
 import base64
 from os import environ as env
 from dotenv import load_dotenv, find_dotenv
+from flask import session
 from openapi_server.exceptions import AuthError
 
 # Load .env file
@@ -26,6 +28,18 @@ def get_secret_hash(username):
     message = username + COGNITO_CLIENT_ID
     dig = hmac.new(bytearray(COGNITO_CLIENT_SECRET, 'utf-8'), msg=message.encode('utf-8'), digestmod='sha256').digest()
     return base64.b64encode(dig).decode()
+
+# Get user attributes from Cognito response
+def get_user_attr(user_data):
+    email = None
+    for attribute in user_data['UserAttributes']:
+        if attribute['Name'] == 'email':
+            email = attribute['Value']
+            break
+
+    return {
+      'email': email
+    }
 
 
 def signup():  # noqa: E501
@@ -53,3 +67,47 @@ def signup():  # noqa: E501
               }, 401)
 
     return response
+
+def signin():
+    # Validate request data
+    if connexion.request.is_json:
+        body = connexion.request.get_json()
+
+    secret_hash = get_secret_hash(body['email'])
+
+    # initiate authentication
+    try:
+        response = userClient.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow='USER_PASSWORD_AUTH',
+            AuthParameters={
+                'USERNAME': body['email'],
+                'PASSWORD': body['password'],
+                'SECRET_HASH': secret_hash
+            }
+        )
+    except Exception as e:
+        code = e.response['Error']['Code']
+        description = e.response['Error']['Message']
+        raise AuthError({
+                  "code": code, 
+                  "description": description
+              }, 401)
+              
+    access_token = response['AuthenticationResult']['AccessToken']
+    refresh_token = response['AuthenticationResult']['RefreshToken']
+
+    # retrieve user data
+    user_data = userClient.get_user(AccessToken=access_token)
+
+    # create user object from user data
+    user = get_user_attr(user_data)
+
+    # set refresh token cookie
+    session['refresh_token'] = refresh_token
+
+    # return user data json
+    return {
+        'token': access_token,
+        'user': user
+    }
