1c4adf602f665255865366e56f79305d5d560d81
Shubha Rajan <shubharajan94@gmail.com>
2022-05-09 21:52:51 -0700

    rm old models, schemas, and generated classes

--- api/openapi_server/models/__init__.py
+++ api/openapi_server/models/__init__.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.api_response import ApiResponse
 from openapi_server.models.applicant_type import ApplicantType
-from openapi_server.models.guest import Guest
-from openapi_server.models.host import Host
 from openapi_server.models.housing_program import HousingProgram
 from openapi_server.models.housing_program_service_provider import HousingProgramServiceProvider
 from openapi_server.models.intake_question import IntakeQuestion
