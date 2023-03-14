52d024f2b34a8768b41d0f5ed25d070275a17b58
Emily Eldar <eldaremily@gmail.com>
2023-03-13 17:17:36 -0700

    Remove testing change

--- api/openapi_server/models/database.py
+++ api/openapi_server/models/database.py
@@ -11,7 +11,6 @@ class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True)
-    blah = Column(String)
 
 
 class ApplicantType(Base):
