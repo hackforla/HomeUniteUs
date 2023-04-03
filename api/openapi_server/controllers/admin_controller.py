f4f7a75d52c920ca31825a4b0447af5da2fb7289
Stephen Barkley-Yeung <sbarkleyyeung@gmail.com>
2023-04-02 19:41:43 -0700

    upgrading initial sign in method
    added in docstring, body checks and updated exception handling.

--- api/openapi_server/controllers/admin_controller.py
+++ api/openapi_server/controllers/admin_controller.py
@@ -5,6 +5,7 @@ import hmac
 import base64
 import requests
 
+from api.openapi_server.controllers import auth_controller
 from os import environ as env
 from dotenv import load_dotenv, find_dotenv
 from flask import redirect, request, session, redirect
@@ -22,21 +23,34 @@ COGNITO_USER_POOL_ID=env.get('COGNITO_USER_POOL_ID')
 COGNITO_REDIRECT_URI = env.get('COGNITO_REDIRECT_URI')
 SECRET_KEY=env.get('SECRET_KEY')
 
-def get_secret_hash(username):
-    message = username + COGNITO_CLIENT_ID
-    dig = hmac.new(bytearray(COGNITO_CLIENT_SECRET, 'utf-8'), msg=message.encode('utf-8'), digestmod='sha256').digest()
-    return base64.b64encode(dig).decode()
+
 
 userClient = boto3.client('cognito-idp', region_name=COGNITO_REGION)
 
-# description: Removes auto generated password and replaces with user assigned password. Used for account setup.
-def inital_sign_in_reset_password():
-    # check for json in request body
-    if connexion.request.is_json:
-        body = connexion.request.get_json()
+def initial_sign_in_reset_password(): 
+    """Sets initial password.
+     
+    Removes auto generated password and replaces with 
+    user assigned password. Used for account setup.
 
-    secret_hash = get_secret_hash(body['email'])
+    Returns: 
+        Response object or exception
+    """
     try:
+        # check for json in request body. If not Json throws an error
+        if connexion.request.is_json:
+            body = connexion.request.get_json()
+        else:
+            raise AuthError({"message": "bad response from server"},500)
+
+        if "email" not in body:
+            raise AuthError({"message": "bad response, no email"},500)
+        
+        if "password" not in body:
+            raise AuthError({"message": "bad response, no password"},500)
+        
+        secret_hash = auth_controller.get_secret_hash(body['email'])
+    
         # call forgot password method
         response = userClient.respond_to_auth_challenge(
             ClientId=COGNITO_CLIENT_ID,
@@ -48,9 +62,6 @@ def inital_sign_in_reset_password():
             
         )
     except Exception as e:
-        print(e)
-        raise AuthError({
-                  "message": e.response['Error']['Message']
-              }, 500)  
+        raise AuthError({"message": "failed to change password"}, 500) from e
     
     return response
