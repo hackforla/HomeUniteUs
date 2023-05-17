6eb8e510809d2453a13231dc10afafa2291ec736
ju1es <jsulpico@gmail.com>
2023-05-16 21:18:23 -0700

    decouple create service provider
    update gitignore
    
    create example of decoupling
    
    remove unused import
    
    update dependencies
    
    create initial test
    
    add tests for service provider repository

--- api/openapi_server/controllers/service_provider_controller.py
+++ api/openapi_server/controllers/service_provider_controller.py
@@ -1,15 +1,12 @@
-
+# Third Party
 import connexion
-import six
 import traceback
 
-from openapi_server.models.api_response import ApiResponse  # noqa: E501
+# Local
 from openapi_server.models.service_provider import ServiceProvider  # noqa: E501
-from openapi_server.models.service_provider_with_id import ServiceProviderWithId
-from openapi_server.models import database as db
-from sqlalchemy.orm import Session
+from openapi_server.repositories.service_provider_repository import HousingProviderRepository
 
-db_engine = db.DataAccessLayer.get_engine()
+housing_provider_repository = HousingProviderRepository()
 
 def create_service_provider():  # noqa: E501
     """Create a housing program service provider
@@ -18,26 +15,19 @@ def create_service_provider():  # noqa: E501
 
     :rtype: ServiceProviderWithId
     """
-    if connexion.request.is_json:
-        try:
-            # The auto-generated ServiceProvider provides 
-            # validation of required columns
-            provider = ServiceProvider.from_dict(
-                connexion.request.get_json()).to_dict()  
-        except ValueError:
-            return traceback.format_exc(ValueError), 400
-        with Session(db_engine) as session:
-            row = db.HousingProgramServiceProvider(
-                provider_name=provider["provider_name"]
-            )
-
-            session.add(row)
-            session.commit()
-
-            provider["id"] = row.id
-            return ServiceProviderWithId.from_dict(provider), 201
-    else:
+    if not connexion.request.is_json:
         return "Bad Request", 400
+    
+    try:
+        # The auto-generated ServiceProvider provides 
+        # validation of required columns
+        provider = ServiceProvider.from_dict(
+            connexion.request.get_json()).to_dict()  
+    except ValueError:
+        return traceback.format_exc(ValueError), 400
+    
+    withId = housing_provider_repository.createServiceProvider(provider)
+    return withId, 201
 
 
 def delete_service_provider(provider_id):  # noqa: E501
@@ -50,15 +40,8 @@ def delete_service_provider(provider_id):  # noqa: E501
 
     :rtype: None
     """
-    with Session(db_engine) as session:
-        query = session.query(
-                db.HousingProgramServiceProvider).filter(
-                    db.HousingProgramServiceProvider.id == provider_id)
-        if query.first() != None:
-            query.delete()
-            session.commit()
-            return f"Service Provider with id {provider_id} successfully deleted", 200
-
+    housing_provider_repository.delete_service_provider(provider_id)
+    return f"Service Provider with id {provider_id} successfully deleted", 200
 
 def get_service_provider_by_id(provider_id):  # noqa: E501
     """Get details about a housing program service provider from an ID
@@ -70,37 +53,18 @@ def get_service_provider_by_id(provider_id):  # noqa: E501
 
     :rtype: ServiceProviderWithId
     """
-    with Session(db_engine) as session:
-        row = session.get(
-            db.HousingProgramServiceProvider, provider_id)
-        if row != None:
-            provider = ServiceProvider(
-                provider_name=row.provider_name).to_dict()
-            
-            provider["id"] = row.id
-            return ServiceProviderWithId.from_dict(provider), 200
-        else:
-            return "Not Found", 404
-
+    provider = housing_provider_repository.get_service_provider_by_id(provider_id)
+    return provider, 200 if provider != None else "Not Found", 404
 
 def get_service_providers():  # noqa: E501
     """Get a list of housing program service providers.
 
      # noqa: E501
 
-
     :rtype: List[ServiceProviderWithId]
     """
-    resp = []
-    with Session(db_engine) as session:
-        table = session.query(db.HousingProgramServiceProvider).all()
-        for row in table:
-            provider = ServiceProvider(
-                provider_name=row.provider_name).to_dict()
-            
-            provider["id"] = row.id
-            resp.append(ServiceProviderWithId.from_dict(provider))
-    return resp, 200
+    providers = housing_provider_repository.get_service_providers()
+    return providers, 200
 
 
 def update_service_provider(provider_id):  # noqa: E501
@@ -113,25 +77,16 @@ def update_service_provider(provider_id):  # noqa: E501
 
     :rtype: ServiceProviderWithId
     """
-    if connexion.request.is_json:
-        try:
-            # The auto-generated ServiceProvider provides 
-            # validation of required columns
-            provider = ServiceProvider.from_dict(
-                connexion.request.get_json()).to_dict() 
-        except ValueError:
-            return traceback.format_exc(ValueError), 400
-        with Session(db_engine) as session:
-            query = session.query(
-                db.HousingProgramServiceProvider).filter(
-                    db.HousingProgramServiceProvider.id == provider_id)
-            if query.first() != None:
-                query.update(provider)
-                session.commit()
-                provider["id"] = provider_id
-                return ServiceProviderWithId.from_dict(provider), 200
-            else:
-                return "Not Found", 404
-    else:
+    if not connexion.request.is_json:
         return "Bad Request", 400
-
+    
+    try:
+        # The auto-generated ServiceProvider provides 
+        # validation of required columns
+        provider = ServiceProvider.from_dict(
+            connexion.request.get_json()).to_dict() 
+    except ValueError:
+        return traceback.format_exc(ValueError), 400
+    
+    updated = housing_provider_repository.update_service_provider(provider, provider_id)
+    return updated, 200 if updated != None else "Not Found", 404
