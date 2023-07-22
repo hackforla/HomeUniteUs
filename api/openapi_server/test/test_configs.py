049034cdfe717b6ede126cef81a0d9d1fba021eb
aMetallurgist <joshua.d1434@gmail.com>
2023-07-22 16:08:19 -0700

    Fix typo. Rename _compile_config to compile_config

--- api/openapi_server/test/test_configs.py
+++ api/openapi_server/test/test_configs.py
@@ -2,7 +2,7 @@
 import pytest
 
 # Local
-from configs.configs import Config, _compile_config
+from configs.configs import Config, compile_config
 
 class TestConfigs:
 
@@ -20,10 +20,10 @@ class TestConfigs:
 
     def test_compile_config_no_personal(self):
         with pytest.raises(ModuleNotFoundError):
-            _compile_config('personal', 'configs.doesnt_exist', 'PersonalConfigExample')
+            compile_config('personal', 'configs.doesnt_exist', 'PersonalConfigExample')
         
     def test_compile_config_some_personal(self):
-        configs = _compile_config('personal', 'configs.personal_config_example', 'PersonalConfigExample')
+        configs = compile_config('personal', 'configs.personal_config_example', 'PersonalConfigExample')
 
         assert configs.HOST == 'localhost'
         assert configs.PORT == 8081
