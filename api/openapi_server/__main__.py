bc813d86969569103210f42520d4d28df84e8ada
Mr. Paul <mrpaul@aestheticwisdom.com>
2023-07-24 13:30:35 +0700

    refactor(api): Make application available to a WSGI server
    The module and function `openapi_server.__main__.py:create_app()` can be used by
    a WSGI server to run the application.
    
    Related #465

--- api/openapi_server/__main__.py
+++ api/openapi_server/__main__.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
-
 # Standard Lib
 from os import environ as env
-from dotenv import load_dotenv, find_dotenv, get_key
 from pathlib import Path
 from typing import Dict, Any
 
-
 # Third Party
 import connexion
+from dotenv import load_dotenv, find_dotenv, get_key
 import prance
 
 # Local
@@ -19,39 +16,67 @@ from configs.configs import compile_config
 from configs.config_properties import ConfigProperties as cp
 
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
-SECRET_KEY=env.get('SECRET_KEY')
-
 DataAccessLayer.db_init()
+
 
 def get_bundled_specs(main_file: Path) -> Dict[str, Any]:
     parser = prance.ResolvingParser(str(main_file.absolute()), lazy=True, strict=True)
     parser.parse()
-    
+
     return parser.specification
 
 
-def main():    
-    app = connexion.App(__name__)
+def create_app():
+    '''
+    Creates a configured application that is ready to be run.
 
-    # Set configs
-    env_config_profile = get_key(find_dotenv(), 'CONFIG_PROFILE')
-    app.app.config.from_object(compile_config(env_config_profile))
-    app.app.json_encoder = encoder.JSONEncoder
-    app.app.secret_key = SECRET_KEY
-    app.add_api(get_bundled_specs(app.get_root_path() / Path('./openapi/openapi.yaml')),
+    The application is configured from external environment variables stored
+    in either a local `.env` in a development environment or from environment
+    variables that are configured prior to running the application.
+
+    This function is made available at the module level so that it can
+    be loaded by a WSGI server in a production-like environment with
+    `openapi_server.__main__:create_app()`.
+    '''
+
+    # `connexion.App` is aliased to `connexion.FlaskApp` (as of connexion v2.13.1)
+    # which is the connexion layer built on top of Flask. So we think of it as
+    # the connexion application.
+    connexion_app = connexion.App(__name__)
+
+    connexion_app.add_api(get_bundled_specs(app.get_root_path() / Path('./openapi/openapi.yaml')),
                 arguments={'title': 'Home Unite Us'},
-                pythonic_params=True)             
-    app.add_error_handler(AuthError, handle_auth_error)
-    app.run(
-        host=app.app.config[cp.HOST.name],
-        port=app.app.config[cp.PORT.name],
-        debug=app.app.config[cp.DEBUG.name],
-        use_reloader=app.app.config[cp.USE_RELOADER.name]
-        )
+                pythonic_params=True)
+    connexion_app.add_error_handler(AuthError, handle_auth_error)
+
+    ENV_FILE = find_dotenv()
+    if ENV_FILE:
+        load_dotenv(ENV_FILE)
+    SECRET_KEY = env.get("SECRET_KEY")
+    env_config_profile = get_key(find_dotenv(), "CONFIG_PROFILE")
+
+    # The underlying instance of Flask is stored in `connexion_app.app`.
+    # This is an instance of `flask.Flask`.
+    flask_app = connexion_app.app
+    flask_app.json_encoder = encoder.JSONEncoder
+    flask_app.secret_key = SECRET_KEY
+
+    # Below, the Flask configuration handler is loaded with the
+    # application configuration settings
+    flask_app.config.from_object(compile_config(env_config_profile))
+
+    return connexion_app
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    # This will/should be only run in a development environment
+
+    connexion_app = create_app()
+    flask_app = connexion_app.app
+
+    connexion_app.run(
+        host=flask_app.config[cp.HOST.name],
+        port=flask_app.config[cp.PORT.name],
+        debug=flask_app.config[cp.DEBUG.name],
+        use_reloader=flask_app.config[cp.USE_RELOADER.name],
+    )
