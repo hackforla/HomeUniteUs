8dcfcebb42ce1b7ddf9856559ae073a5f7130267
agosmou <agosmou@gmail.com>
2023-07-28 23:49:37 -0700

    adds tests skeleton

---
+++ api/openapi_server/test/test_guest_dashboard_controller.py
@@ -0,0 +1,13 @@
+# Third Part
+
+
+# Local
+
+
+class TestGuestDashboardController:
+    
+    def test_get_tasks_no_result():
+        pass
+
+    def test_get_tasks_with_result():
+        pass
---
+++ api/openapi_server/test/test_guest_dashboard_repository.py
@@ -0,0 +1,13 @@
+# Third Party
+
+
+# Local
+
+
+class TestGuestDashboardRepository:
+
+    def test_get_tasks_no_result():
+        pass
+
+    def test_get_tasks_with_result():
+        pass
