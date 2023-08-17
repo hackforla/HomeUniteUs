dde9d4dbf431f962de1e44310d7816f1d6b95bc6
aMetallurgist <joshua.d1434@gmail.com>
2023-08-16 21:00:46 -0700

    Delete unused model file

--- api/openapi_server/test/test_service_provider_controller.py
+++ api/openapi_server/test/test_service_provider_controller.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import
 
 from flask import json
 from six import BytesIO
-
-from openapi_server.models.api_response import ApiResponse  # noqa: E501
 from openapi_server.models.database import HousingProgramServiceProvider  # noqa: E501
 from openapi_server.test import BaseTestCase
 
