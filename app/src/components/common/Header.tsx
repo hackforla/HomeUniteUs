3ae7a6efc94f34ded5195aab6e2a216401b27cea
Jose Padilla <jose.apadilla99@gmail.com>
2023-09-28 19:46:46 -0500

    feat: added prelogin navigation design with sidebar on smaller screens

--- app/src/components/common/Header.tsx
+++ app/src/components/common/Header.tsx
@@ -3,20 +3,25 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import {Stack} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Drawer, IconButton, Stack, styled} from '@mui/material';
 import logo from '../../img/favicon.png';
-import {useLocation} from 'react-router-dom';
+
+const MOBILE_DRAWER_WIDTH = 209;
 
 const navItems = [
   {title: 'Login', href: '/signin'},
   {title: 'Sign Up', href: '/signup'},
 ];
 
-export const Header = () => {
-  const location = useLocation();
-  const pathsToHideNavItems = ['/signin', '/signup'];
+const container = window !== undefined ? () => window.document.body : undefined;
 
-  const shouldHideItems = pathsToHideNavItems.includes(location.pathname);
+export const Header = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(prevState => !prevState);
+  };
 
   return (
     <Box sx={{display: 'flex'}}>
@@ -37,27 +42,74 @@ export const Header = () => {
               alt="Home Unite Us logo"
             />
           </Stack>
-          {!shouldHideItems ? (
-            <Stack direction="row" gap={1} sx={{alignItems: 'center'}}>
-              <Box sx={{display: 'flex'}}>
-                {navItems.map(({title, href}) => {
-                  const variant = title === 'Sign Up' ? 'contained' : 'text';
-                  return (
-                    <Button
-                      variant={variant}
-                      href={href}
-                      color="primary"
-                      key={title}
-                    >
-                      {title}
-                    </Button>
-                  );
-                })}
-              </Box>
-            </Stack>
-          ) : null}
+          <Stack direction="row" gap={1} sx={{alignItems: 'center'}}>
+            <Box sx={{display: {md: 'flex', sm: 'none', xs: 'none'}}}>
+              {navItems.map(({title, href}) => {
+                const variant = title === 'Sign Up' ? 'contained' : 'text';
+                return (
+                  <Button
+                    variant={variant}
+                    href={href}
+                    color="primary"
+                    key={title}
+                  >
+                    {title}
+                  </Button>
+                );
+              })}
+            </Box>
+          </Stack>
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{height: '40px', width: '40px', display: {md: 'none'}}}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
+        <StyledDrawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            display: {xs: 'block', md: 'none'},
+            zIndex: theme => theme.zIndex.drawer + 2,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          width={MOBILE_DRAWER_WIDTH}
+        >
+          <Box
+            onClick={handleDrawerToggle}
+            sx={{display: 'flex', flexDirection: 'column', mt: 2, gap: 2}}
+          >
+            {navItems.map(({title, href}) => {
+              return (
+                <Button variant="text" href={href} color="primary" key={title}>
+                  {title}
+                </Button>
+              );
+            })}
+          </Box>
+        </StyledDrawer>
       </AppBar>
     </Box>
   );
 };
+
+interface DrawerProps {
+  width: number;
+}
+const StyledDrawer = styled(Drawer, {
+  shouldForwardProp: prop => prop !== 'width',
+})<DrawerProps>(({width}) => ({
+  width: width,
+  flexShrink: 0,
+  [`& .MuiDrawer-paper`]: {
+    width: width,
+    boxSizing: 'border-box',
+  },
+}));
