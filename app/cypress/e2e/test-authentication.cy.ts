f91652371861fa6623b7cb14b1ed68e22cbe6925
aMetallurgist <joshua.d1434@gmail.com>
2023-11-03 17:44:02 -0700

    Add authentication test suite

---
+++ app/cypress/e2e/test-authentication.cy.ts
@@ -0,0 +1,198 @@
+const LOGIN_PAGES_URL_PATTERN: RegExp = /\/(coordinator|host|guest)/;
+const SIGNIN_PAGE_URL: string = Cypress.config().baseUrl + '/signin';
+const PROTECTED_PATHS: string[] = ['/guest', '/host', '/coordinator'] //profile?
+
+function loginUsingUI(password: string, email: string) {
+  cy.findByRole('textbox', {name: /email/i}).type(email);
+  cy.get('#password').type(password);
+  cy.contains('button', 'Sign in').click()
+}
+
+function logoutUsingUI() {
+  cy.get('button[aria-label="Open settings"]').click();
+  cy.contains('p', 'Logout').should('be.visible').click();
+}
+
+describe('Authentication', () => {
+    beforeEach(function() {
+      this.email = Cypress.env('USE_MOCK') ? 'test@gmail.com' : Cypress.env('REAL_EMAIL');
+      this.password = Cypress.env('USE_MOCK') ? 'Test123#' : Cypress.env('REAL_PASSWORD');
+      expect(this.email).is.not.undefined;
+      expect(this.password).is.not.undefined;
+
+      const getIsSignedIn = () => {
+        // You cannot use the cy.url() command within the cy.intercept() handler directly 
+        // because Cypress commands are asynchronous, and cy.intercept() expects a synchronous response.
+        return LOGIN_PAGES_URL_PATTERN.test(window.location.pathname);
+      }
+
+      if (Cypress.env('USE_MOCK')) {
+        const oneHourFromNow = new Date();
+        oneHourFromNow.setHours(oneHourFromNow.getHours() + 1);
+        const cookieExpiration = oneHourFromNow.toUTCString();
+        
+        cy.intercept('POST', '/api/auth/signin', {
+            statusCode: 200,
+            headers: {
+                'Set-Cookie': `session=fake_session_value; expires=${cookieExpiration}; path=/`
+            },
+            body: {
+              "token": "fake.jwt.token",
+              "user": {
+                "email": "test@gmail.com"
+              }
+            }
+          }).as('signin');
+        cy.intercept('POST', '/api/auth/signout', {
+            statusCode: 200,
+            headers: {
+                "Set-Cookie": "session=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; HttpOnly; Path=/"
+            }
+          }).as('signout');
+          cy.intercept('GET', '/api/auth/session', (req) => {
+            req.reply({
+              statusCode: getIsSignedIn() ? 200 : 401
+            });
+          }).as('session');
+          
+          cy.intercept('GET', '/api/auth/refresh', (req) => {
+            req.reply({
+              statusCode: getIsSignedIn() ? 200 : 401
+            });
+          }).as('refresh');
+        cy.intercept('GET', '/api/auth/user', {
+          statusCode: 200,
+          body: {
+            "token": "fake.jwt.token",
+            "user": {
+              "email": "test@gmail.com"
+            }
+          }
+        }).as('user');
+      } else {
+          cy.intercept('POST', '/api/auth/signin').as('signin');
+          cy.intercept('POST', '/api/auth/signout').as('signout');
+          cy.intercept('GET', '/api/auth/session').as('session');
+          cy.intercept('GET', '/api/auth/refresh').as('refresh');
+          cy.intercept('GET', '/api/auth/user').as('user');
+      }
+    });
+
+    it('should log the user in, show login page', function() {
+      cy.visit('/signin');
+      loginUsingUI(this.password, this.email);
+      
+      cy.wait('@signin').then((intercept) => {
+        const body = intercept.response?.body
+        const headers = intercept.response?.headers
+
+        expect(body).to.have.property('user')
+        expect(body.user).to.have.property('email', this.email)
+        expect(body).to.have.property('token');
+        expect(body.token).to.be.a('string').and.not.to.be.empty;
+      })
+
+      cy.url().should('match', LOGIN_PAGES_URL_PATTERN);
+    });
+
+    it('should signin, signout then prevent accessing authenticated pages', function() {
+      cy.visit('/signin');
+      loginUsingUI(this.password, this.email);
+      cy.wait('@signin').its('response.statusCode').should('be.within', 200, 299);
+      logoutUsingUI();
+      cy.url().should('eq', SIGNIN_PAGE_URL);
+      for (const path of PROTECTED_PATHS) {
+        cy.visit(path);
+        cy.url().should('eq', SIGNIN_PAGE_URL);
+      }
+    });
+
+    it('should prevent navigation to authenticated pages before signin', function() {
+      for (const path of PROTECTED_PATHS) {
+        cy.visit(path);
+        cy.url().should('eq', SIGNIN_PAGE_URL);
+      }
+    });
+
+    it('should stay logged in without redirecting', function() {
+      cy.visit('/signin');
+      loginUsingUI(this.password, this.email);
+      
+      cy.wait('@signin').its('response.statusCode').should('be.within', 200, 299);
+
+      let signinURL: string;
+      cy.url().then((url) => {
+        expect(url).to.match(LOGIN_PAGES_URL_PATTERN, 'The URL should end with /coordinator, /host, or /guest');
+        signinURL = url;
+      });
+
+      cy.wait(750);
+      
+      cy.url().then((url) => {
+        expect(url).to.equal(signinURL, 'Signin page redirected shortly after signin!');
+      });
+    });
+  
+    it('should receive a session cookie at signin and send cookie with requests', function() {      
+      cy.getCookie('session').should('not.exist');
+
+      cy.visit('/signin'); 
+
+      cy.wait('@session')
+        .its('response.statusCode')
+        .should('eq', 401);
+      cy.wait('@refresh')
+        .its('response.statusCode')
+        .should('eq', 401);
+      
+      cy.findByRole('textbox', {name: /email/i}).type(this.email);
+      cy.get('#password').type(this.password);
+      cy.contains('button', 'Sign in').click()
+  
+      cy.wait('@signin').then((intercept) => {
+        const body = intercept.response?.body
+        const setCookieHeader = intercept.response?.headers['set-cookie']
+        expect(body).to.have.property('user')
+        expect(body.user).to.have.property('email', this.email);
+        expect(setCookieHeader).to.exist;
+        expect(setCookieHeader).to.be.an('array').with.length(1);
+        if (setCookieHeader) {
+          expect(setCookieHeader[0]).to.include('session=');
+        } else {
+          throw new Error('set-cookie header is not set');
+        }
+      })
+
+      cy.url().should('match', LOGIN_PAGES_URL_PATTERN);
+    });
+
+  it('should have access to user data after signin', function() {
+    cy.visit('/signin');
+    loginUsingUI(this.password, this.email);
+    cy.wait('@signin').its('response.statusCode').should('be.within', 200, 299);
+    cy.wait('@user').then((intercept) => {
+      expect(intercept.response?.statusCode).eq(200);
+      const body = intercept.response?.body;
+      expect(body).to.have.property('user');
+      expect(body.user).to.have.property('email');
+    });
+  });
+
+  it('should remained logged in after a page refresh', function() {
+
+    if (Cypress.env('USE_MOCK')) {
+      // Mocking the exact refresh and session endpoint behavior is difficult
+      // Leave this test for real credentials
+      this.skip();
+    }
+
+    cy.visit('/signin');
+    loginUsingUI(this.password, this.email);
+    cy.url().should('match', LOGIN_PAGES_URL_PATTERN);
+    cy.reload();
+    cy.wait(500);
+    cy.url().should('match', LOGIN_PAGES_URL_PATTERN);
+  });
+
+  });
+  
