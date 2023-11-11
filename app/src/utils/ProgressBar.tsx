c79646f42db22163e318602a3f361a8e2f7753b2
Jose Padilla <jose.apadilla99@gmail.com>
2023-11-11 01:32:08 -0600

    feat: implemented progress bar tracker

---
+++ app/src/utils/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import {styled} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import LinearProgress, {
+  linearProgressClasses,
+} from '@mui/material/LinearProgress';
+
+const BorderLinearProgress = styled(LinearProgress)(({theme}) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+  },
+}));
+
+export default function ProgressBar({
+  progressBarValue,
+}: {
+  progressBarValue: number;
+}) {
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <BorderLinearProgress variant="determinate" value={progressBarValue} />
+    </Box>
+  );
+}
