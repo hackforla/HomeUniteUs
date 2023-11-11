750d6668126afc7a9c91d2f7d9a8fd405d54e252
Jose Padilla <jose.apadilla99@gmail.com>
2023-11-10 22:40:59 -0600

    feat: initial yup validation needs for guest application

---
+++ app/src/utils/GuestApplicationSchema.ts
@@ -0,0 +1,57 @@
+import {array, date, object, string} from 'yup';
+
+export const GuestApplicationSchema = object({
+  fullName: string().required(),
+  dateOfBirth: date()
+    .required()
+    .default(() => new Date()),
+  gender: string().required(),
+  email: string().email(),
+  phoneNumber: string()
+    .required()
+    .matches(
+      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+      'Phone number is not valid.',
+    ),
+  contactPrefrence: string().required(),
+  nameOfGuestArray: array().of(
+    object({
+      nameOfGuest: string().required('Name is Required'),
+    }),
+  ),
+  typeOfPetArray: array().of(
+    object({
+      typeOfPet: string().required('Pet Type is Required'),
+    }),
+  ),
+  currentlyEmployed: string().required(),
+  employmentSearch: string().when('currentlyEmployed', {
+    is: (currentlyEmployed: string) => currentlyEmployed === 'No',
+    then: string().required(),
+  }),
+  employmentDescription: string().when('currentlyEmployed', {
+    is: (currentlyEmployed: string) => currentlyEmployed === 'Yes',
+    then: string().required(),
+  }),
+  educationEnrollment: string().required(),
+  educationEnrollmentSearch: string().when('educationEnrollment', {
+    is: (educationEnrollment: string) => educationEnrollment === 'No',
+    then: string().required(),
+  }),
+  educationProgramName: string().when('educationEnrollment', {
+    is: (educationEnrollment: string) => educationEnrollment === 'Yes',
+    then: string().required(),
+  }),
+  multilingual: string().required(),
+  spokenLanguages: string().when('multilingual', {
+    is: (multilingual: string) => multilingual === 'Yes',
+    then: string().required(),
+  }),
+  cigarettesUse: string().required(),
+  alcoholUse: string().required(),
+  otherSubstanceUse: string().required(),
+  mentalIllness: string().required(),
+  programGoals: string().required(),
+  hostRelationshipGoal: string().required(),
+  potentialChallenges: string().required(),
+});
