a30e1688a6d7131558a6952129879579d146a016
Erik <eguntner2291@gmail.com>
2024-01-18 12:20:40 -0800

    Upgrade MSW to v2

--- app/src/utils/test/handlers.ts
+++ app/src/utils/test/handlers.ts
@@ -1,11 +1,14 @@
-import {rest} from 'msw';
+import {http, HttpResponse} from 'msw';
 
 const handlers = [
-  rest.post(/.*\/api\/auth\/forgot_password$/, (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
-  rest.post(/.*\/api\/auth\/invite$/, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.post(
+    `${import.meta.env.VITE_HUU_API_BASE_URL}/auth/forgot_password`,
+    () => {
+      return new HttpResponse();
+    },
+  ),
+  http.post(`${import.meta.env.VITE_HUU_API_BASE_URL}/auth/invite`, () => {
+    return new HttpResponse();
   }),
 ];
 
