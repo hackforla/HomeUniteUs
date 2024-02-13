e68a6009b2b6032a3b71828e1b0611c4c30d7907
mira-kine <mirak0419@gmail.com>
2024-02-12 21:48:08 -0800

    passing SignUpForm tests for special characters

--- app/src/components/__tests__/SignUpForm.test.tsx
+++ app/src/components/__tests__/SignUpForm.test.tsx
@@ -21,15 +21,12 @@ const prepare = () => {
   const emailInput = screen.getByLabelText(/email/i);
   const passwordInput = screen.getByLabelText('Password');
   const submitButton = screen.getByRole('button', {name: /sign up/i});
-  const passwordValidation = screen.getByLabelText(
-    'password must contain at least one special character',
-  );
+
   return {
     onSubmit,
     emailInput,
     passwordInput,
     submitButton,
-    passwordValidation,
   };
 };
 
@@ -43,22 +40,22 @@ describe('<SignUpForm />', () => {
 
   // when correct special characters are added, it should pass validation form
   it('should pass password validation form with correct special characters', () => {
-    const {passwordInput, passwordValidation} = prepare();
-    fireEvent.change(passwordInput, {target: {value: 'test123!'}});
-    expect(passwordValidation).not.toBeInTheDocument();
+    const {passwordInput, submitButton} = prepare();
+    fireEvent.change(passwordInput, {target: {value: 'Test123!'}});
+    expect(submitButton).not.toBeDisabled;
   });
 
   // when special character not valid, password validation should be present
   it('should display error text when special character not present', () => {
-    const {passwordInput, passwordValidation} = prepare();
+    const {passwordInput, submitButton} = prepare();
     fireEvent.change(passwordInput, {target: {value: 'test123'}});
-    expect(passwordValidation).toBeInTheDocument();
+    expect(submitButton).toBeDisabled;
   });
 
   // test for unusual special characters
   it('should pass password validation form with unusual special characters', () => {
-    const {passwordInput, passwordValidation} = prepare();
+    const {passwordInput, submitButton} = prepare();
     fireEvent.change(passwordInput, {target: {value: '7væVPj¼±mó5÷ÙÞW'}});
-    expect(passwordValidation).toBeInTheDocument();
+    expect(submitButton).not.toBeDisabled;
   });
 });
