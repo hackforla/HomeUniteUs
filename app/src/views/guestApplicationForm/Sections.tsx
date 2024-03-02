7108f2428d3a95ba37fa96cab283fc17b872b8d9
Jose Padilla <jose.apadilla99@gmail.com>
2024-03-01 21:54:12 -0600

    single styled section

--- app/src/views/guestApplicationForm/Sections.tsx
+++ app/src/views/guestApplicationForm/Sections.tsx
@@ -1,22 +1,44 @@
-import {Stack, Typography} from '@mui/material';
+import {Box, Stack, Typography} from '@mui/material';
 import {FormContainer} from '../../components/authentication/FormContainer';
+import {CircleOutlined} from '@mui/icons-material';
 
 export const Sections = () => {
   return (
     <FormContainer>
-      <Stack
-        spacing={4}
-        sx={{justifyContent: 'center', alignItems: 'center', width: '100%'}}
-      >
-        <Typography variant="h4" sx={{fontWeight: 'bold'}}>
-          Profile Sections
-        </Typography>
+      <Stack spacing={4} sx={{width: '100%'}}>
         <Stack
-          component="form"
-          title="Sections Page"
-          sx={{width: '100%', alignItems: 'flex-start'}}
-          spacing={4}
-        ></Stack>
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Typography sx={{fontSize: 20, fontWeight: 'medium'}}>
+            Profile Sections
+          </Typography>
+          <Typography sx={{fontSize: 14, fontWeight: 'medium'}}>
+            0 of 11
+          </Typography>
+        </Stack>
+        <Stack sx={{display: 'flex', gap: 1}}>
+          <Box
+            sx={{
+              borderRadius: 2,
+              backgroundColor: 'white',
+              height: 56,
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingInline: 3,
+            }}
+          >
+            <Typography sx={{fontSize: 16, fontWeight: 'medium'}}>
+              Basic Information
+            </Typography>
+            <CircleOutlined color="action" />
+          </Box>
+        </Stack>
       </Stack>
     </FormContainer>
   );
