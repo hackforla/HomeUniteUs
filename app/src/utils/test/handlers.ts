9c7101950ee542a04e3876ee93228764ed557608
Erik <eguntner2291@gmail.com>
2024-03-14 11:30:28 -0700

    Add helper function to build url in test env

--- app/src/utils/test/handlers.ts
+++ app/src/utils/test/handlers.ts
@@ -1,13 +1,15 @@
 import {http, HttpResponse} from 'msw';
 
+const buildUrl = (path: string) => {
+  const baseUrl = `${import.meta.env.VITE_HUU_API_BASE_URL}`;
+  return `${baseUrl}${path}`;
+};
+
 const handlers = [
-  http.post(
-    `${import.meta.env.VITE_HUU_API_BASE_URL}/auth/forgot_password`,
-    () => {
-      return new HttpResponse();
-    },
-  ),
-  http.post(`${import.meta.env.VITE_HUU_API_BASE_URL}/auth/invite`, () => {
+  http.post(buildUrl('/auth/forgot_password'), () => {
+    return new HttpResponse();
+  }),
+  http.post(buildUrl('/auth/invite'), () => {
     return new HttpResponse();
   }),
 ];
