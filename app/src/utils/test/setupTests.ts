84299cd584d41bd84db184dff126d4b4feeb3104
Erik <eguntner2291@gmail.com>
2024-04-03 14:49:26 -0700

    Remove whatwg-fetch and node-fetch packages

--- app/src/utils/test/setupTests.ts
+++ app/src/utils/test/setupTests.ts
@@ -1,9 +1,6 @@
 // Handle test setup here
 import '@testing-library/jest-dom';
-import nodeFetch, {Request, Response} from 'node-fetch';
 import {server} from './server';
-
-Object.assign(global, {fetch: nodeFetch, Request, Response});
 
 beforeAll(() => server.listen({onUnhandledRequest: 'error'}));
 afterEach(() => server.resetHandlers());
