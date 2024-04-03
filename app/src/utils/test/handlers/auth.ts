e925aeb225360834a3f29a50b48d3328579e9a68
Erik <eguntner2291@gmail.com>
2024-04-03 15:24:27 -0700

    Remove base implementation

--- app/src/utils/test/handlers/auth.ts
+++ app/src/utils/test/handlers/auth.ts
@@ -1,15 +1,10 @@
 import {http, HttpResponse} from 'msw';
 
-const buildUrl = (path: string) => {
-  const baseUrl = `${import.meta.env.VITE_HUU_API_BASE_URL}`;
-  return `${baseUrl}${path}`;
-};
-
 export const handlers = [
-  http.post(buildUrl('/auth/forgot_password'), () => {
+  http.post('/auth/forgot_password', () => {
     return new HttpResponse();
   }),
-  http.post(buildUrl('/auth/invite'), () => {
+  http.post('/auth/invite', () => {
     return new HttpResponse();
   }),
 ];
