133fcfdd358a88d9a450760ec635ee1c5f9efcae
Erik <eguntner2291@gmail.com>
2024-04-04 13:18:34 -0700

    Differentiate fields for host profile

--- app/src/utils/test/db/profile.ts
+++ app/src/utils/test/db/profile.ts
@@ -168,27 +168,6 @@ export const intakeProfiles: GetProfileApiResponse[] = [
               required: true,
             },
           },
-          {
-            id: faker.string.numeric(4),
-            title: 'Gender',
-            type: 'dropdown',
-            properties: {
-              choices: [
-                {id: faker.string.numeric(4), label: 'Woman'},
-                {id: faker.string.numeric(4), label: 'Man'},
-                {id: faker.string.numeric(4), label: 'Questioning'},
-                {id: faker.string.numeric(4), label: 'Transgender'},
-                {id: faker.string.numeric(4), label: 'Non-binary'},
-                {
-                  id: faker.string.numeric(4),
-                  label: 'Doesn’t know or prefers not to answer	',
-                },
-              ],
-            },
-            validations: {
-              required: true,
-            },
-          },
         ],
       },
       {
@@ -213,24 +192,12 @@ export const intakeProfiles: GetProfileApiResponse[] = [
       },
       {
         id: faker.string.numeric(4),
-        title: 'Other Guests and/or Pets',
+        title: 'Home Information',
         fields: [
           {
             id: faker.string.numeric(4),
-            title: 'Additional Guests',
-            type: 'additional_guests',
-            properties: {},
-            validations: {},
-          },
-        ],
-      },
-      {
-        id: faker.string.numeric(4),
-        title: 'Employment Information',
-        fields: [
-          {
-            id: faker.string.numeric(4),
-            title: 'Are you currently employed?',
+            title:
+              'Do you have an extra bedroom or private space in their home?',
             type: 'yes_no',
             properties: {},
             validations: {
@@ -240,17 +207,76 @@ export const intakeProfiles: GetProfileApiResponse[] = [
           {
             id: faker.string.numeric(4),
             title:
-              'If no, are you currently looking for work? If so, what type?',
-            type: 'short_text',
+              'Are you able to provide Guest with access to a kitchen in which to prepare meals, store food and access to shared or private bathroom?',
+            type: 'yes_no',
             properties: {},
-            validations: {},
+            validations: {
+              required: true,
+            },
           },
           {
             id: faker.string.numeric(4),
-            title: 'If yes, please describe your current employment',
-            type: 'short_text',
+            title:
+              'Can you commit to hosting youth Guest for at least 3-6 months?',
+            type: 'yes_no',
             properties: {},
-            validations: {},
+            validations: {
+              required: true,
+            },
+          },
+        ],
+      },
+      {
+        id: faker.string.numeric(4),
+        title: 'Restrictions',
+        fields: [
+          {
+            id: faker.string.numeric(4),
+            title: 'Do you or anyone in your houshold smoke?',
+            type: 'yes_no',
+            properties: {},
+            validations: {
+              required: true,
+            },
+          },
+          {
+            id: faker.string.numeric(4),
+            title: 'Is smoking allowed inside your home?',
+            type: 'yes_no',
+            properties: {},
+            validations: {
+              required: true,
+            },
+          },
+          {
+            id: faker.string.numeric(4),
+            title: 'Do you or anyone in your household drink alcohol?',
+            type: 'yes_no',
+            properties: {},
+            validations: {
+              required: true,
+            },
+          },
+          {
+            id: '9846',
+            title: 'Do you have concerns about your drinking?',
+            type: 'yes_no',
+            properties: {},
+            validations: {
+              required: true,
+            },
+          },
+          {
+            id: faker.string.numeric(4),
+            title: 'If yes, please explain why you are concerned',
+            type: 'long_text',
+            properties: {},
+            validations: {
+              required_if: {
+                field_id: '9846',
+                value: 'yes',
+              },
+            },
           },
         ],
       },
