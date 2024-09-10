cb118beafb51b897cb7ad3e34e600dd19f1ab23d
Mr. Paul <mrpaul@aestheticwisdom.com>
2024-09-10 15:50:07 -0700

    fastapi-migration: Fix bad merge

--- api-v2/app/intake_profile/controller.py
+++ api-v2/app/intake_profile/controller.py
@@ -2,7 +2,8 @@ import logging
 
 from fastapi import Depends, APIRouter, HTTPException, Response, Security
 from fastapi.responses import RedirectResponse
-from api.deps import (
+
+from app.api.deps import (
     get_db,
     get_cognito_client,
     requires_auth,
@@ -13,22 +14,22 @@ from api.deps import (
 router = APIRouter()
 
 
-@router.post("/guest/")
-def post_guest_intake_profile(body, guest: Depends(aim_guest)):
-    forms_repo = FormsRepository(DataAccessLayer.session())
+# @router.post("/guest/")
+# def post_guest_intake_profile(body, guest: Depends(aim_guest)):
+#     forms_repo = FormsRepository(DataAccessLayer.session())
 
-    form_id = forms_repo.add_form(body)
-    form = forms_repo.get_form_json(form_id)
-    if form:
-        return form, 200
-    return {}, 404
+#     form_id = forms_repo.add_form(body)
+#     form = forms_repo.get_form_json(form_id)
+#     if form:
+#         return form, 200
+#     return {}, 404
 
 
-@router.get("/guest/{form_id}")
-def get_guest_intake_profile(form_id, guest: Depends(aim_guest)):
-    forms_repo = FormsRepository(DataAccessLayer.session())
+# @router.get("/guest/{form_id}")
+# def get_guest_intake_profile(form_id, guest: Depends(aim_guest)):
+#     forms_repo = FormsRepository(DataAccessLayer.session())
 
-    form = forms_repo.get_form_json(form_id)
-    if form:
-        return form, 200
-    return f"Form with id {form_id} does not exist.", 404
+#     form = forms_repo.get_form_json(form_id)
+#     if form:
+#         return form, 200
+#     return f"Form with id {form_id} does not exist.", 404
