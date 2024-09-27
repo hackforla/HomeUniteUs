75a847dde5bec75f7bde85966eb666cdaf3d10f5
Erik <eguntner2291@gmail.com>
2024-09-27 11:09:29 -0700

    Style section home page

--- frontend/src/features/intake-profile/ProfileSectionList.tsx
+++ frontend/src/features/intake-profile/ProfileSectionList.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import {
   Stack,
-  List,
-  ListItem,
-  ListItemButton,
   Typography,
   Container,
   Box,
+  Link,
+  LinkProps,
 } from '@mui/material';
 import {InProgressIcon} from '../ui/icons/InProgressIcon';
 import LockIcon from '@mui/icons-material/Lock';
 import {ArrowForwardIos, CheckCircleOutlined} from '@mui/icons-material';
-import {useParams} from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import {Link as RouterLink, useParams} from 'react-router-dom';
 
 import {useGetProfileQuery} from '../../services/profile';
 import {Loading} from '../ui';
 
-const icon = {
+const statusIcon = {
   complete: (
     <CheckCircleOutlined
       color="success"
@@ -48,6 +46,24 @@ const icon = {
   partial: <InProgressIcon />,
 };
 
+const SECTION_GAP = 110;
+
+const placementRight: {[key: number]: number} = {
+  0: 240,
+  1: 80,
+  2: 0,
+  3: 60,
+  4: 180,
+};
+
+const placementLeft: {[key: number]: number} = {
+  0: 130,
+  1: 20,
+  2: 100,
+  3: 60,
+  4: 240,
+};
+
 export const ProfileSectionList = () => {
   const {profileId} = useParams();
 
@@ -59,52 +75,107 @@ export const ProfileSectionList = () => {
   if (isLoading || profileData === undefined) return <Loading />;
 
   const {fieldGroups} = profileData;
+  const rightFieldGroups = fieldGroups.slice(0, fieldGroups.length / 2);
+  const leftFieldGroups = fieldGroups.slice(fieldGroups.length / 2);
 
   return (
-    <Container maxWidth="sm">
-      <Stack component={List} sx={{px: 2, gap: 2, width: '100%'}}>
-        {fieldGroups.map(({id, title}) => {
+    <Container maxWidth="lg">
+      <Typography variant="h4" sx={{textAlign: 'center', mt: 4, mb: 6}}>
+        We want to get to know you better
+      </Typography>
+
+      <Stack
+        sx={{
+          position: 'relative',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img src="/images/guest-welcome.svg" />
+        {rightFieldGroups.map(({id, title}, index) => {
           // Change status here to see different styles
           // complete | partial | incomplete | locked
-          const status = 'complete';
-          const fieldTitle = title || '...';
-
+          const status = 'incomplete';
           return (
-            <ListItem key={id} disableGutters disablePadding>
-              <ListItemButton
-                to={`${id}`}
-                component={Link}
+            <SectionLink
+              key={id}
+              id={id}
+              sx={{
+                top: index * SECTION_GAP,
+                right: placementRight[index],
+              }}
+            >
+              {statusIcon[status]}
+              <Typography
                 sx={{
-                  borderRadius: 2,
-                  backgroundColor: 'primary.main',
-                  color: 'white',
-                  display: 'flex',
-                  alignItems: 'center',
-                  height: '48px',
-                  width: '150px',
-                  gap: 2,
-                  paddingInline: 3,
-                  textDecoration: 'none',
-                  '&:hover': {
-                    backgroundColor: 'primary.dark',
-                  },
+                  fontSize: 16,
+                  color: `palette.text.primary`,
                 }}
               >
-                {icon[status]}
-                <Typography
-                  sx={{
-                    fontSize: 16,
-                    color: `palette.text.primary`,
-                  }}
-                >
-                  {fieldTitle}
-                </Typography>
-                <ArrowForwardIos sx={{ml: 'auto'}} />
-              </ListItemButton>
-            </ListItem>
+                {title}
+              </Typography>
+              <ArrowForwardIos sx={{ml: 'auto', fontSize: 'medium'}} />
+            </SectionLink>
+          );
+        })}
+        {leftFieldGroups.map(({id, title}, index) => {
+          // Change status here to see different styles
+          // complete | partial | incomplete | locked
+          const status = 'incomplete';
+          return (
+            <SectionLink
+              key={id}
+              id={id}
+              sx={{
+                top: index * SECTION_GAP,
+                left: placementLeft[index],
+              }}
+            >
+              {statusIcon[status]}
+              <Typography
+                sx={{
+                  fontSize: 16,
+                  color: `palette.text.primary`,
+                }}
+              >
+                {title}
+              </Typography>
+              <ArrowForwardIos sx={{ml: 'auto', fontSize: 'medium'}} />
+            </SectionLink>
           );
         })}
       </Stack>
     </Container>
+  );
+};
+
+interface SectionLinkProps extends LinkProps {
+  children: React.ReactNode;
+  id: string;
+}
+
+const SectionLink = ({children, id, ...props}: SectionLinkProps) => {
+  return (
+    <Link
+      to={`${id}`}
+      component={RouterLink}
+      sx={{
+        ...props.sx,
+        display: 'flex',
+        position: 'absolute',
+        borderRadius: '99999px',
+        backgroundColor: 'primary.main',
+        color: 'white',
+        alignItems: 'center',
+        padding: 2,
+        gap: 2,
+        '&:hover': {
+          backgroundColor: 'primary.dark',
+        },
+      }}
+    >
+      {children}
+    </Link>
   );
 };
