c45758892e03abbf8f417964eec1ec2f5dca9b66
Mr. Paul <mrpaul@aestheticwisdom.com>
2024-09-28 20:01:24 -0700

    fastapi-migration: Migrate health endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from .health import health_router
 from app.modules.router import api_router
 import app.core.db as db
 import app.core.config as config
@@ -18,3 +19,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router, prefix="/api")
+app.include_router(health_router, prefix="/api/health", tags=["health"])
