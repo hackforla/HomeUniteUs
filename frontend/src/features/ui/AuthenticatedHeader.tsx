0c1dbb245334ea2f15eb5a3aaeeba1443947a824
Mugdh Chauhan <42421946+mugdhchauhan@users.noreply.github.com>
2025-07-04 18:16:20 +0100

    add user id to contact_info table

--- frontend/src/features/ui/AuthenticatedHeader.tsx
+++ frontend/src/features/ui/AuthenticatedHeader.tsx
@@ -121,6 +121,7 @@ const AvatarDropdownMenu = () => {
   // ?? as the initials for debugging purposes
   const user = useSelector(selectCurrentUser) || {
     role: {} as UserRole,
+    id: 0,
     email: 'unknown',
     firstName: '?',
     lastName: '?',
@@ -130,7 +131,11 @@ const AvatarDropdownMenu = () => {
     <Box sx={{ flexGrow: 0 }}>
       <Tooltip title="Open settings">
         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-          <Avatar alt={user.firstName + user.lastName}>{getInitials(user)}</Avatar>
+          {/* <Avatar alt={user.firstName + user.lastName}>{getInitials(user)}</Avatar> */}
+          <Avatar alt={`${user.firstName} ${user.lastName}`}>
+            {user.id ? user.id.toString() : getInitials(user)}
+          </Avatar>
+
         </IconButton>
       </Tooltip>
       <Menu
--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -5,6 +5,7 @@ export interface UserRole {
 }
 
 export interface User {
+  id: number;
   email: string;
   firstName: string;
   lastName: string;
